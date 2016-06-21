ac6a2efb331d82954c458d0d6247ff9f71d30449
Dani <i_d_ani@hotmail.com>
2016-06-21 11:53:11 +0200

    Ampliadas funcionalidades de almacenado de tickets en la BBDD.

--- tfgProject/www/js/ocrTicketController.js
+++ tfgProject/www/js/ocrTicketController.js
@@ -18,7 +18,6 @@ angular.module('starter.controllers')
 
     //Definimos la función que realiza un threshold sobre la imagen de caman
     Caman.Filter.register("threshold", function (adjust) {
-      console.log("Entra threshold");
       return this.process("threshold", function (rgba) {
         var luminance = (0.2126 * rgba.r) + (0.7152 * rgba.g) + (0.0722 * rgba.b)
 
@@ -44,65 +43,87 @@ angular.module('starter.controllers')
     $scope.exposicion = 0;
 
     $scope.threshold = 120;
+    var cacheThreshold = -1; //Esta variable se emplea para no leer dos veces la misma imagen si el threshold se mantiene
     $scope.htmlLectura = "";
 
     $scope.mostrarPopup = false;
+    $scope.mostrarPopup2 = false;
+    $scope.elegirImagen = true;
+    $scope.mostrarImagen = true;
+
+    $scope.mostrando = false;
+
+    $scope.ticket = {};
 
     var numLineas = 0;
 
     $scope.capturarTexto = function(){
       console.log("Poh vamo a ver que");
+      if(cacheThreshold==$scope.threshold){
+        $scope.mostrarPopup = true;
+      }else{
+        cacheThreshold=$scope.threshold;
+        var textoTicket = "";
 
-      var textoTicket = "";
+        /*
+         var canvas = document.createElement('canvas');
+         var context = canvas.getContext('2d');
+         var img = document.getElementById('escaner');
+         context.drawImage(img, 0, 0 );
+         var imagen = context.getImageData(0, 0, img.width, img.height);
+         */
+        //var imagen = document.getElementById('escaner');
+        var imagen = document.getElementById('canvasAux');
 
-      /*
-       var canvas = document.createElement('canvas');
-       var context = canvas.getContext('2d');
-       var img = document.getElementById('escaner');
-       context.drawImage(img, 0, 0 );
-       var imagen = context.getImageData(0, 0, img.width, img.height);
-       */
-      //var imagen = document.getElementById('escaner');
-      var imagen = document.getElementById('canvasAux');
+        Tesseract
+          .recognize( imagen, {
+            lang: 'spa'} )
+          .then( function(d){
+            textoTicket = d;
+            numLineas = 0;
+            var array = d.text.split('\n');
+            var texto = "";
+            /*
+             var texto = "<h2>Líneas del ticket</h2>";
+             texto += "<p><i ng-click='mostrarAyuda()' class='ayuda fa fa-question-circle'></i></p>";
+             */
+            for(var i=0; i<array.length; i++){
+              if(array[i]!="") {
+                texto += '<select id="selLinea'+numLineas+'" name="sel'+numLineas+'">' +
+                  '<option value="des">Línea desechable</option>' +
+                  '<option value="evento">Evento</option>' +
+                  '<option value="resultado">Resultado</option>' +
+                  '<option value="tipo_resultado">Tipo/Resultado</option>' +
+                  '<option value="hora_evento">Fecha/Hora evento</option>' +
+                  '<option value="importe">Importe apostado</option>' +
+                  '<option value="premio">Premio</option>' +
+                  '</select>';
 
-      Tesseract
-        .recognize( imagen, {
-          lang: 'spa'} )
-        .then( function(d){
-          textoTicket = d;
-          numLineas = 0;
-          var array = d.text.split('\n');
-          console.log("------------------");
-          var texto = "";
-          /*
-          var texto = "<h2>Líneas del ticket</h2>";
-          texto += "<p><i ng-click='mostrarAyuda()' class='ayuda fa fa-question-circle'></i></p>";
-          */
-          for(var i=0; i<array.length; i++){
-            console.log("Línea "+i);
-            console.log(array[i]);
-            if(array[i]!="") {
-              texto += '<select id="selLinea'+i+'" name="sel'+i+'">' +
-                '<option value="des">Línea desechable</option>' +
-                '<option value="evento">Evento</option>' +
-                '<option value="resultado">Resultado</option>' +
-                '<option value="hora_evento">Hora evento</option>' +
-                '<option value="importe">Importe apostado</option>' +
-                '<option value="premio">Premio</option>' +
-                '</select>';
-              //texto += '<input type="checkbox" id="check' + i + '"/>';
-              texto += '<input type="text" id="linea' + i + '" value="' + array[i] + '" /><br />';
-              numLineas++;
+                texto += '<input type="text" id="linea' + numLineas + '" value="' + array[i] + '" /><br />';
+                numLineas++;
+              }
             }
-          }
 
-          //texto += '<button id="cancelarTicket" class="cancelar">Cancelar</button><button id="aceptarTicket" class="aceptar">Aceptar</button>'
-          console.log(texto);
-          $scope.mostrarTexto(texto);
-        } );
+            //texto += '<button id="cancelarTicket" class="cancelar">Cancelar</button><button id="aceptarTicket" class="aceptar">Aceptar</button>'
+            $scope.mostrarTexto(texto);
+          } );
+      }
+    }
+
+    $scope.limpiarCanvas = function(){
+      var c = document.getElementById("canvasAux");
+      var context = c.getContext("2d");
+
+      context.clearRect(0, 0, c.width, c.height);
     }
 
     $scope.reset = function(){
+      if($scope.mostrando != true){
+        $scope.$apply(function() {
+          $scope.mostrando = true;
+        });
+      }
+
       var c = document.getElementById("canvasAux");
       var ctx = c.getContext("2d");
       var img = document.getElementById("escaner");
@@ -114,10 +135,18 @@ angular.module('starter.controllers')
       });
     }
 
+    $scope.reiniciar = function(){
+
+      $scope.mostrando = false;
+      document.getElementById("escaner").src = "";
+      $scope.limpiarCanvas();
+      $scope.imgURI = "";
+      location.reload();
+    }
 
     $scope.aplicarThreshold = function(t){
       //$scope.reset();
-      console.log(t);
+      $scope.threshold = t;
       Caman("#canvasAux", function () {
         this.revert();
         this.threshold(t).render();
@@ -135,15 +164,12 @@ angular.module('starter.controllers')
 
         this.revert();
         if (brillo != -1) {
-          console.log("Cambiando brillo: " + brillo);
           this.brightness(brillo); // valores entre -100 y 100
         }
         if (contraste != -1){
-          console.log("Cambiando contraste: " + contraste);
           this.contrast(contraste); // valores entre -100 y 100
         }
         if (exposicion != -1) {
-          console.log("Cambiando exposición: " + exposicion);
           this.exposure(exposicion); // valores entre -100 y 100
         }
 
@@ -163,7 +189,6 @@ angular.module('starter.controllers')
       };
 
       $cordovaCamera.getPicture(options).then(function (imageData) {
-        console.log("OK");
         $scope.imgURI = "data:image/jpeg;base64," + imageData;
         $scope.reset();
       }, function (err) {
@@ -178,6 +203,66 @@ angular.module('starter.controllers')
       $scope.$apply(function() {
         $scope.mostrarPopup = true;
       });
+    }
+
+    $scope.devuelveFecha = function(){
+      var fecha = new Date();
+      var dia = fecha.getDate().toString()
+      var mes = (fecha.getMonth()+1).toString();
+      if(mes.length==1){
+        mes = "0"+mes;
+      }
+      var anyo = fecha.getFullYear().toString();
+
+      var hora = fecha.getHours().toString();
+      if(hora.length==1){
+        hora = "0"+hora;
+      }
+
+      var minutos = fecha.getMinutes().toString();
+      if(minutos.length==1){
+        minutos="0+minutos";
+      }
+
+      var textoFecha = dia + "/" + mes + "/" + anyo+" "+hora+":"+minutos;
+      return textoFecha;
+    }
+
+    //Cambia el contenido del texto del popup para permitir al usuario almacenar el ticket
+    $scope.guardarTicket = function(n, p){
+      if(n!="" && n!=null){
+        $scope.ticket.nombre = n;
+      }else{
+        $scope.ticket.nombre = $scope.devuelveFecha();
+      }
+
+      if(p=="" || p==null){
+        $scope.ticket.proveedor = p;
+      }else{
+        $scope.ticket.proveedor = "Sin proveedor";
+      }
+
+
+      var response = $http.post('http://eyebetapi.herokuapp.com/api/tickets/', $scope.ticket);
+      response.success(function (data) {
+        console.log(data);
+        swal(
+          {
+            title: "¡Ticket guardado!",
+            text: "Su ticket ha sido almacenado correctamente.",
+            type: 'success',
+            timer: 2500,
+            showConfirmButton: false
+          }, function(){
+            console.log("Se ha cerrado");
+          }
+        );
+      });
+    }
+
+    $scope.volverATicket = function(){
+      $scope.mostrarPopup2 = false;
+      $scope.mostrarPopup = true;
     }
 
     $scope.choosePhoto = function () {
@@ -223,33 +308,200 @@ angular.module('starter.controllers')
 
     $scope.cerrarPopup = function(){
       $scope.mostrarPopup = false;
-      $scope.$apply();
     }
 
     //Una vez obtenidas las líneas del ticket tal y como están en él, podemos hacer las llamadas a la API de datos
     $scope.tratarDatos = function(){
       console.log("A tratar!");
+      var linea="";
+      var contEvento = 0;
+      var arrayEventos = [];
+      var evento = {};
+      var ticket = {};
       for(var i=0; i<numLineas; i++){
-        if(document.getElementById("check"+ i.toString()).checked){
-          //Ya tengo las
-          alert(document.getElementById("linea"+ i.toString()).value);
+        console.log(i);
+        linea = document.getElementById("linea"+i.toString()).value;
+        //alert(document.getElementById("linea"+ i.toString()).value);
+
+        switch(document.getElementById("selLinea"+ i.toString()).value){
+          case "evento":
+            if(contEvento!=0) {
+              console.log("Evento"+contEvento+": ");
+              console.log(evento);
+              arrayEventos.push(evento);
+              evento = {};
+            }
+            var equipos = tratarEvento(linea);
+            console.log("EVENTO "+contEvento);
+            for(var j=0; j<equipos.length; j++){
+              equipos[j];
+              console.log("equipo "+j+": "+equipos[j]);
+              switch(j){
+                case 0: evento.equipo1 = eliminarEspacios(equipos[j]); break;
+                case 1: evento.equipo2 = eliminarEspacios(equipos[j]); break;
+                case 2: evento.equipo3 = eliminarEspacios(equipos[j]); break;
+                case 3: evento.equipo4 = eliminarEspacios(equipos[j]); break;
+                case 4: evento.equipo5 = eliminarEspacios(equipos[j]); break;
+                case 5: evento.equipo6 = eliminarEspacios(equipos[j]); break;
+                default: evento.equipox = eliminarEspacios(equipos[j]); break;
+              }
+            }
+            contEvento++;
+            evento.texto = linea;
+            break;
+          case "resultado":
+            var array = tratarResultado(linea);
+
+            if(array.length==1){//Resultado
+              evento.resultado = eliminarEspacios(array[0]);
+            }else if(array.length==2){//Resultado y cuota
+              evento.resultado = eliminarEspacios(array[0]);
+              evento.cuota = parseFloat(eliminarEspacios(array[1]).replace(')', '').replace(',', '.'));
+            }else if(array.length==3){//Tipo de apuesta, resultado y cuota
+              evento.tipo = eliminarEspacios(array[0]);
+              evento.resultado = eliminarEspacios(array[1]);
+              evento.cuota = parseFloat(eliminarEspacios(array[2]).replace(')', '').replace(',', '.'));
+            }
+            break;
+          case "tipo_resultado":
+            var array = tratarResultado(linea);
+
+            if(array.length==1){//Resultado
+              evento.resultado = eliminarEspacios(array[0]);
+            }else if(array.length==2){//tipo y resultado
+              evento.tipo = eliminarEspacios(array[0]);
+              evento.resultado = eliminarEspacios(array[1]);
+            }else if(array.length==3){//Tipo, resultado y cuota
+              evento.tipo = eliminarEspacios(array[0]);
+              evento.resultado = eliminarEspacios(array[1]);
+              evento.cuota = parseFloat(eliminarEspacios(array[2]).replace(')', '').replace(',', '.'));
+            }
+
+            break;
+          case "hora_evento": evento.hora = linea; break;
+          case "importe": ticket.importe = parseFloat(tratarDinero(linea).replace(',', '.')); break;
+          case "premio": ticket.premio = parseFloat(tratarDinero(linea).replace(',', '.')); break;
+          default: break;
         }
       }
-
+      if(evento!={}){
+        arrayEventos.push(evento);
+      }
+      ticket.eventos = arrayEventos;
+      console.log(ticket);
+      $scope.ticket = ticket;
+      $scope.mostrarPopup=false;
+      $scope.mostrarPopup2=true;
     }
 
     $scope.mostrarAyuda = function(){
       var explicacion = "<div style='text-align:left;'><span class='spanPrin'>Para que analicemos correctamente el ticket, debes:</span> <br /><br /> " +
         "<span class='spanSec'><b>-</b>Corregir los errores que se hayan producido</span>" +
-        "<span class='spanSec'><b>-</b>Indicar el tipo de contenido de cada línea</span>" +
+        "<span class='spanSec'><b>-</b>Indicar el tipo de contenido de cada línea de la lista desplegable</span>" +
         "<span class='spanSec'><b>-</b>Dejar los eventos en el formato <b>Equipo1 - Equipo2</b>. Ej.: Real Madrid-F.C. Barcelona</span>" +
-        "<span class='spanSec'><b>-</b>Dejar sólo los resultados de evento en las líneas que correspondan a estos.</span>"
+        "<span class='spanSec'><b>-</b>Dejar sólo los resultados de evento en las líneas que correspondan a estos. Ej.: <b>1X</b> o <b>Empate</b>. En caso de que también se desee analizar la cuota del evento, encerrar entre paréntesis. Ej.: <b>1X (1.80)</b></span>"+
+        "<span class='spanSec'><b>-</b>Si la línea de resultado incluye también tipo de evento (<b>1X2</b>, <b>Marcador exacto</b>, etc) separar del resto de igual forma mediante '@'. Ej.: <b>Marcador exacto @ 2:0 (1.80)</b></span>";
       swal({
         title: "Instrucciones",
         text: explicacion,
         html: true,
         allowOutsideClick: true,
       });
+    }
+
+    function tratarEvento(linea){
+      var arrayEquipos = linea.split('—');
+      if(arrayEquipos.length == 1){
+        arrayEquipos = linea.split('-');
+        if(arrayEquipos.length == 1){
+          arrayEquipos = linea.split('/');
+          if(arrayEquipos.length == 1){
+            arrayEquipos = linea.split('<');
+            if(arrayEquipos.length == 1){
+              arrayEquipos = linea.split('>');
+              if(arrayEquipos.length == 1){
+                arrayEquipos = linea.split('»');
+              }
+              if(arrayEquipos.length == 1){
+                arrayEquipos = linea;
+              }
+            }
+          }
+        }
+      }
+      return arrayEquipos;
+    }
+
+    function tratarResultado(linea){
+      //División para quedarse con la cuota, en caso de que este
+      var arrayEquipos = [];
+      arrayEquipos = linea.split('(');
+      var cuota = "";
+      if(arrayEquipos.length>1){
+        cuota=arrayEquipos[1];
+        linea=arrayEquipos[0];
+      }
+
+      arrayEquipos = linea.split('@');
+      if(arrayEquipos.length == 1){
+        arrayEquipos = linea.split('(');
+        if(arrayEquipos.length == 1){
+          arrayEquipos = linea.split('—');
+          if(arrayEquipos.length == 1){
+            arrayEquipos = linea.split('-');
+            if(arrayEquipos.length == 1){
+              arrayEquipos = linea.split('>');
+              if(arrayEquipos.length == 1){
+                arrayEquipos = linea.split('»');
+              }
+              if(arrayEquipos.length == 1){
+                arrayEquipos = linea;
+              }
+            }
+          }
+        }
+      }
+
+      if(cuota!="" && cuota!=null){
+        console.log(typeof(arrayEquipos));
+        console.log(arrayEquipos);
+        if(typeof(arrayEquipos)=="string"){
+          var aux = String(arrayEquipos);
+          arrayEquipos = [aux, cuota];
+        }else{
+          arrayEquipos.push(cuota);
+        }
+      }
+      return arrayEquipos;
+    }
+
+    function tratarDinero(linea){
+      var devuelve = linea.replace('€', '');
+      devuelve = devuelve.replace('EUR', '');
+      devuelve = devuelve.replace(/[^0-9.,]/g, "");
+      devuelve = eliminarEspacios(devuelve);
+
+      return devuelve;
+    }
+
+    function eliminarEspacios(palabra){
+      console.log(palabra);
+      if(palabra!=null){
+        while(palabra.length>0 && palabra[0]==' '){
+          palabra = palabra.slice(1,palabra.length);
+        }
+
+        while(palabra.length>0 && palabra[palabra.length-1]==' '){
+          palabra = palabra.slice(0, palabra.length-1);
+        }
+      }
+      return palabra;
+    }
+
+    function equipararTiposDeApuesta(tipo){
+      switch(removeDiacritics(tipo)){
+
+      }
     }
 
     document.getElementById("fichero").onchange = function(){
